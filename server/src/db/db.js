5cd01585bb206ee07a71e42768e24c56e2a7a502
Gustavo Gutierrez <imguss@gmail.com>
2026-03-18 22:29:47 -0700

    added db connection

---
+++ server/src/db/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const DATABASE_URL = process.env.DATABASE_URL
+
+const pool = new Pool({
+  connectionString: DATABASE_URL
+})
+
+export default pool
