d129ea9bef4ef60097bdb1f4b82c911523538e0e
Gustavo Gutierrez <imguss@gmail.com>
2026-03-23 19:41:35 -0400

    fixed error handling

--- server/src/controllers/artistController.js
+++ server/src/controllers/artistController.js
@@ -1,18 +1,48 @@
 import { createNewArtist, getAllArtists, getArtistById } from '../models/artistModel.js'
 
 export async function getArtists(req, res) {
-  const artists = await getAllArtists()
-  res.json(artists)
+  try {
+    const artists = await getAllArtists()
+  
+    if (!artists) {
+      return res.status(404).json({error: 'No artists found'})
+    }
+  
+    res.json(artists)
+    
+  } catch (err) {
+    res.status(500).json({error: 'Artists not found'})
+  }
 }
 
 export async function getArtist(req, res) {
-  const { id } = req.params
-
-  const artist = await getArtistById(id)
-  res.json(artist)
+  try {
+    const { id } = req.params
+  
+    const artist = await getArtistById(id)
+  
+    if (!artist) {
+      return res.status(404).json({error: 'Artist not found'})
+    }
+  
+    res.json(artist)
+    
+  } catch (err) {
+    res.status(500).json({error: 'Internal server error'})
+  }
 }
 
 export async function createArtist(req, res) {
-  const newArtist = await createNewArtist(req.body)
-  res.json(newArtist)
+  try {
+    console.log(req.body)
+    const newArtist = await createNewArtist(req.body)
+
+    res.json(newArtist)
+    
+  } catch (err) {
+    console.error(err)
+    res.status(err.status || 500).json({
+      error: err.message || 'Internal server error'
+    })
+  }
 }
