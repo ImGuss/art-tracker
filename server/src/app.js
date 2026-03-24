aff57ed2686920a91847c9d3db26dc865c59f2a6
Gustavo Gutierrez <imguss@gmail.com>
2026-03-23 20:03:05 -0400

    added error handling middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { artistRouter } from './routes/artistRoutes.js'
+import { errorHandler } from './middleware/errorHandler.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/artists', artistRouter)
+
+app.use(errorHandler)
 
 export default app
