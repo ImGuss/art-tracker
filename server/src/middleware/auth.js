99db8899cb8f1f254c02400a42360597ca9afd79
Gustavo Gutierrez <imguss@gmail.com>
2026-03-27 16:24:23 -0700

    added auth middleware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken'
+
+import { AppError } from '../utils/AppError.js'
+import { findUserById } from '../models/userModel.js'
+
+export async function protect(req, res, next) {
+  const token = req.cookies.token
+  try {
+
+    if (!token) {
+      return next(new AppError('Session cookie does not exist', 401))
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const userId = decoded.userId
+
+    const user = await findUserById(userId)
+
+    if (!user) {
+      return next(new AppError('Could not find user', 401))
+    }
+
+    req.user = user
+
+    next()
+
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return next(new AppError('Your session has expired, pleas log in again', 401))
+    }
+    if (err.name === 'JsonWebTokenError') {
+      return next(new AppError('Invalid session, please log in again', 401))
+    }
+    next(err)
+  }
+}
