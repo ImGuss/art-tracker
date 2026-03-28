05f447f99da27b7ae522f19fbe4f2135f165f09c
Gustavo Gutierrez <imguss@gmail.com>
2026-03-28 13:12:43 -0700

    removed unecessary try catch blocks

--- server/src/models/artworkModel.js
+++ server/src/models/artworkModel.js
@@ -1,50 +1,31 @@
 import pool from '../db/db.js'
 
 export async function getAllArtworks() {
-  try {
-    const res = await pool.query(`SELECT * FROM artworks`)
-
-    return res.rows
-
-  } catch (err) {
-    throw err
-  }
+  const res = await pool.query(`SELECT * FROM artworks`)
+  return res.rows
 }
 
 export async function getArtworkById(id) {
-  try {
-    const res = await pool.query(
-      `
-        SELECT * FROM artworks
-        WHERE id = $1
-      `, [id]
-    )
+  const res = await pool.query(
+    `
+      SELECT * FROM artworks
+      WHERE id = $1
+    `, [id]
+  )
 
-    if (res.rows.length === 0) return null
-
-    return res.rows[0]
-
-  } catch (err) {
-    throw err
-  }
+  return res.rows[0] || null
 }
 
 export async function createNewArtwork(body) {
-  const { title, artistId, yearCreated, medium, imageUrl } = body
+  const { title, artist_id, museum_id, year_created, medium, image_url } = body
 
-  try {
+  const res = await pool.query(
+    `
+      INSERT INTO artworks (title, artist_id, museum_id, year_created, medium, image_url)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING *
+    `, [title, artist_id, museum_id, year_created, medium, image_url]
+  )
 
-    const res = await pool.query(
-      `
-        INSERT INTO artworks (title, artist_id, year_created, medium, image_url)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING *
-      `, [title, artistId, yearCreated, medium, imageUrl]
-    )
-
-    return res.rows[0]
-
-  } catch (err) {
-    throw err
-  }
+  return res.rows[0]
 }
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -2,49 +2,42 @@ import pool from '../db/db.js'
 
 export async function createUser(body) {
   const { username, email, passHash } = body
-  try {
-    const newUser = await pool.query(
-      `
-        INSERT INTO users (username, email, password_hash)
-        VALUES ($1, $2, $3)
-        RETURNING id, username, email, created_at 
-      `, [username, email, passHash]
-    )
 
-    return newUser.rows[0]
+  const newUser = await pool.query(
+    `
+      INSERT INTO users (username, email, password_hash)
+      VALUES ($1, $2, $3)
+      RETURNING id, username, email, created_at 
+    `, [username, email, passHash]
+  )
 
-  } catch (err) {
-    throw err
-  }
+  return newUser.rows[0]
 }
 
 export async function findUserByEmail(email) {
-  try {
-    
-    const user = await pool.query(
-      `SELECT * FROM users WHERE email = $1`,
-      [email]
-    )
-  
-    return user.rows[0] || null
+  const user = await pool.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email]
+  )
 
-  } catch (err) {
-    throw err
-  }
+  return user.rows[0] || null
+}
+
+export async function findUserByUsername(username) {
+  const user = await pool.query(
+    `SELECT id, username, email, created_at FROM users WHERE username = $1`,
+    [username]
+  )
+
+  return user.rows[0] || null
 }
 
 export async function findUserById(id) {
-  try {
-    
-    const user = await pool.query(
-      `SELECT id, username, email, created_at
-      FROM users WHERE id = $1`,
-      [id]
-    )
-  
-    return user.rows[0] || null
-    
-  } catch (err) {
-    throw err
-  }
+  const user = await pool.query(
+    `SELECT id, username, email, created_at
+    FROM users WHERE id = $1`,
+    [id]
+  )
+
+  return user.rows[0] || null
 }
