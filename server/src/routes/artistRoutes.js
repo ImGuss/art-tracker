c868a6a16e66866a361eb6383a8eb62522bd2f0c
Gustavo Gutierrez <imguss@gmail.com>
2026-03-29 13:58:57 -0700

    added validation to routes

--- server/src/routes/artistRoutes.js
+++ server/src/routes/artistRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import { protect } from '../middleware/auth.js'
+
 import { getArtist, getArtists, createArtist } from '../controllers/artistController.js'
 
 export const artistRouter = express.Router()
@@ -7,4 +9,4 @@ export const artistRouter = express.Router()
 artistRouter.get('/:id', getArtist)
 artistRouter.get('/', getArtists)
 
-artistRouter.post('/', createArtist)
+artistRouter.post('/', protect, createArtist)
--- server/src/routes/artworkRoutes.js
+++ server/src/routes/artworkRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import { protect } from '../middleware/auth.js'
+
 import { getArtwork, getArtworks, createArtwork } from '../controllers/artworkController.js'
 
 export const artworkRouter = express.Router()
@@ -7,4 +9,4 @@ export const artworkRouter = express.Router()
 artworkRouter.get('/:id', getArtwork)
 artworkRouter.get('/', getArtworks)
 
-artworkRouter.post('/', createArtwork)
+artworkRouter.post('/', protect, createArtwork)
