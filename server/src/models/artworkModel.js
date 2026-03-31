7e73f8f98a0f3dbc3b39754cf915b0bac3300da0
Gustavo Gutierrez <imguss@gmail.com>
2026-03-30 22:55:44 -0700

    added tags to artwork endpoint

--- server/src/models/artworkModel.js
+++ server/src/models/artworkModel.js
@@ -8,8 +8,14 @@ export async function getAllArtworks() {
 export async function getArtworkById(id) {
   const res = await pool.query(
     `
-      SELECT * FROM artworks
-      WHERE id = $1
+      SELECT
+        aw.*,
+        ARRAY_REMOVE(ARRAY_AGG(t.name), NULL) AS tags
+      FROM artworks aw
+      LEFT JOIN artwork_tags awt ON awt.artwork_id = aw.id
+      LEFT JOIN tags t ON awt.tag_id = t.id
+      WHERE aw.id = $1
+      GROUP BY aw.id
     `, [id]
   )
 
