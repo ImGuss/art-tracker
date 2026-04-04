bc9da7fa04f6090c68fe614882ddcfc6d2205574
Gustavo Gutierrez <imguss@gmail.com>
2026-04-03 23:20:37 -0700

    started on artists page fetching

--- client/src/pages/ArtistsPage.tsx
+++ client/src/pages/ArtistsPage.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react'
+import axiosInstance from '../api/axiosInstance'
+
+interface Artist {
+  id: number,
+  name: string,
+  birth_year: number | null,
+  death_year: number | null,
+  birth_place: string | null,
+  description: string | null
+}
+
 export default function ArtistsPage() {
+
+  const [artists, setArtists] = useState<Artist[]>([])
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await axiosInstance.get<Artist[]>('/artists')
+    
+          setArtists(res.data)
+        } catch (err) {
+          setError('Failed to load artists') 
+        }
+      }
+
+      fetchData()
+
+  }, [])
+
+  const artistElements = artists.map(artist => (
+      <div key={artist.id} >{artist.name}</div>
+  ))
+
   return (
-    <div>Artists Page</div>
+    <div>
+      Artists Page
+      {artistElements}
+      { error && <p>{error}</p> }
+    </div>
   )
 }
