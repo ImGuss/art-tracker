101d8e3a01381ca623eecfded297c9c7d7350536
Gustavo Gutierrez <imguss@gmail.com>
2026-04-03 23:20:06 -0700

    continued axios setup

---
+++ client/src/api/axiosInstance.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: '/api',
+  timeout: 1000,
+  headers: {
+    withCredentials: true,
+    "Content-Type": 'application/json'
+  }
+})
+
+export default axiosInstance
