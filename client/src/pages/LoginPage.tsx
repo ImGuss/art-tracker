4502fefc58776cd542987873e1246eb90bdf34f0
Gustavo Gutierrez <imguss@gmail.com>
2026-04-04 23:07:41 -0700

    started on login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,4 +1,31 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+import axiosInstance from '../api/axiosInstance'
+import { useAuth } from '../hooks/useAuth'
+
 const LoginPage = () => {
+  // state values
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState<string | null>(null)
+
+  // hooks
+  const { login } = useAuth()
+  const navigate = useNavigate()
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axiosInstance.post('/auth/login', { email, password })
+  
+      login(res.data)
+  
+      navigate('/')
+    } catch (err) {
+      setError('Failed to login')
+    }
+  }
+
   return (
     <div>Login Page</div>
   )
