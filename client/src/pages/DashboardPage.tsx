4c66c1efe81c4c7f23ec3dabc0f2c0e81e7682a3
Gustavo Gutierrez <imguss@gmail.com>
2026-04-11 23:42:56 -0700

    dashboard testing

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,6 +1,21 @@
+import { Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
+
 const DashboardPage = () => {
+
+  const user = useAuth().user
+
+  if (!user) {
+    return(
+      <div>
+        Dashboard Page
+        <Link to="/login">Login</Link>
+      </div>
+    )
+  }
+
   return (
-    <div>Dashboard Page</div>
+    <div>{user.username}'s Dashboard</div>
   )
 }
 
