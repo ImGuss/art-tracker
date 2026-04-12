ebae4786d90372c0862fdbfcf9bcccb26113b525
Gustavo Gutierrez <imguss@gmail.com>
2026-04-11 23:41:02 -0700

    started on layout and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'
 
 import ProtectedRoute from './components/ProtectedRoute'
+import Layout from './components/Layout'
 
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -18,17 +19,19 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/artists' element={<ArtistsPage />} />
-      <Route path='/artworks' element={<ArtworksPage />} />
-      <Route path='/museums' element={<MuseumsPage />} />
-      <Route path='/tags' element={<TagsPage />} />
-      {/* PROTECTED ROUTES */}
-      <Route path='/' element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-      <Route path='/collections' element={<ProtectedRoute><CollectionsPage /></ProtectedRoute>} />      
-      <Route path='/visits' element={<ProtectedRoute><VisitsPage /></ProtectedRoute>} />
-      {/* <Route path='*' element={<NotFoundPage />} /> */}
+      <Route element={<Layout />}>
+        <Route path='/' element={<DashboardPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/artists' element={<ArtistsPage />} />
+        <Route path='/artworks' element={<ArtworksPage />} />
+        <Route path='/museums' element={<MuseumsPage />} />
+        <Route path='/tags' element={<TagsPage />} />
+        {/* PROTECTED ROUTES */}
+        <Route path='/collections' element={<ProtectedRoute><CollectionsPage /></ProtectedRoute>} />      
+        <Route path='/visits' element={<ProtectedRoute><VisitsPage /></ProtectedRoute>} />
+        {/* <Route path='*' element={<NotFoundPage />} /> */}
+      </Route>
     </Routes>
   )
 }
