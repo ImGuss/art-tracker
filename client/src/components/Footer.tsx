193f514a7d1ff88bfff01c71c278f36df944fe0d
Gustavo Gutierrez <imguss@gmail.com>
2026-04-12 19:54:51 -0700

    added a footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import '../styles/Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 Gustavo Gutierrez</p>
+      <a href="https://github.com/imguss">GitHub</a>
+    </footer>
+  )
+}
+
+export default Footer
