b394d25f4a798112d3f406905460b564e027b073
Gustavo Gutierrez <imguss@gmail.com>
2026-04-14 23:24:35 -0700

    created artist card component

--- client/src/components/ArtistCard.tsx
+++ client/src/components/ArtistCard.tsx
@@ -1,4 +1,8 @@
-import type { Artist } from "../types/artist"
+import { NavLink } from 'react-router'
+
+import type { Artist } from '../types/artist'
+
+import './ArtistCard.css'
 
 interface ArtistCardProps {
   artist: Artist;
@@ -6,8 +10,37 @@ interface ArtistCardProps {
 
 const ArtistCard = ({ artist }: ArtistCardProps) => {
 
+  const {
+    id,
+    name,
+    birth_year,
+    death_year,
+    example_artwork_url
+  } = artist
+
   return (
-    <article>Name is {artist.name}</article>
+    <article className="artist-card">
+      <img
+        className="example-artwork"
+        src={example_artwork_url ?? undefined}
+        alt={`Example painting by ${name}`}
+      />
+      <p className="artist-name">{name}</p>
+      { birth_year ?
+        <div className="artist-dates">
+          <span className="birth-year">{birth_year}</span>
+          <span> - </span> 
+          <span className="death-year">{death_year}</span>
+        </div> :
+        <div className="artist-dates">Unknown</div>
+      }
+      <NavLink
+        className="view-artworks-link"
+        to={`/artists/${id}`}
+      >
+        View Artworks
+      </NavLink>
+    </article>
   )
 }
 
