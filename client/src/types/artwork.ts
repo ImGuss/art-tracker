8c736cbb60bbc3c3a50d8436b6d2f1e2f7c9cbf0
Gustavo Gutierrez <imguss@gmail.com>
2026-04-19 22:52:29 -0700

    added museum id, museum name, and tags to artwork type

--- client/src/types/artwork.ts
+++ client/src/types/artwork.ts
@@ -6,4 +6,7 @@ export interface Artwork {
   year_created: string | null;
   medium: string | null;
   image_url: string | null;
+  museum_id: number | null;
+  museum_name: string | null;
+  tags: string[] | null;
 }
