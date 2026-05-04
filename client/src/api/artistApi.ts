baf8b0fedc1c9c16247d75579e5b47b5a80e8b6f
Gustavo Gutierrez <imguss@gmail.com>
2026-05-03 20:07:07 -0700

    added add artist form logic

--- client/src/api/artistApi.ts
+++ client/src/api/artistApi.ts
@@ -1,4 +1,6 @@
-import axiosInstance from "./axiosInstance";
+import axiosInstance from './axiosInstance'
+
+import type { CreateArtistData } from '../types/artist'
 
 export async function getArtists(limit: number, offset: number, q: string | undefined) {
   const params = new URLSearchParams({
@@ -18,5 +20,11 @@ export async function getArtists(limit: number, offset: number, q: string | unde
 export async function getArtistById(id: number) {
   const res = await axiosInstance.get(`/artists/${id}`)
   
+  return res.data
+}
+
+export async function createArtist(data: CreateArtistData) {
+  const res = await axiosInstance.post(`/artists`, data)
+
   return res.data
 }
--- client/src/components/AddArtistForm.tsx
+++ client/src/components/AddArtistForm.tsx
@@ -1,9 +1,60 @@
-const AddArtistForm = () => {
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+import { createArtist } from '../api/artistApi'
+
+interface AddArtistFormProps {
+  onClose: () => void;
+}
+
+const AddArtistForm = ({onClose}: AddArtistFormProps) => {
+  const [error, setError] = useState<string | null>(null)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const data = new FormData(e.currentTarget)
+
+      const rawBirthYear = data.get('birth_year')
+      const rawDeathYear = data.get('death_year')
+  
+      const name = data.get('name') as string | null
+      const birth_year = rawBirthYear ? Number(rawBirthYear) : null
+      const death_year = rawDeathYear ? Number(rawDeathYear) : null
+      const birth_place = data.get('birth_place') as string | null
+      const description = data.get('description') as string | null
+
+      if (!name) {
+        setError('Artist name is required')
+        return
+      }
+
+      const body = {
+        name,
+        birth_year: birth_year || null,
+        death_year: death_year || null,
+        birth_place: birth_place || null,
+        description: description || null
+      }
+
+      const res = await createArtist(body)
+
+      navigate(`/artists/${res.id}`)
+
+    } catch (err) {
+      setError('Failed to add artist')
+    }
+  }
+
   return (
     <form
       className="modal-form"
+      onSubmit={handleSubmit}
     >
-      <label htmlFor="add-artist-name">Title</label>
+      <label htmlFor="add-artist-name">Name</label>
       <input
         type="text"
         id="add-artist-name"
@@ -36,11 +87,28 @@ const AddArtistForm = () => {
       />
 
       <label htmlFor="add-artist-description">Description</label>
-      <textarea name="description" id="add-artist-description"></textarea>
+      <textarea
+        name="description"
+        id="add-artist-description"
+        rows={4}
+      ></textarea>
+
+      { error && <p className="form-error">{error}</p> }
 
       <div className="modal-btn-container">
-        <button className="gold-outline-btn">Cancel</button>
-        <button className="gold-btn">Add Artist</button>
+        <button
+          className="gold-outline-btn"
+          type="button"
+          onClick={onClose}
+        >
+          Cancel
+        </button>
+        <button
+          className="gold-btn"
+          type="submit"
+        >
+          Add Artist
+        </button>
       </div>
     </form>
   )
