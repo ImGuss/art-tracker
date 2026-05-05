011218d8a3649d99566cbcbcd45e1cc8390b82aa
Gustavo Gutierrez <imguss@gmail.com>
2026-05-05 19:17:59 -0400

    added get visit by id

--- client/src/api/visitApi.ts
+++ client/src/api/visitApi.ts
@@ -8,6 +8,12 @@ export async function getUserVisits() {
   return res.data
 }
 
+export async function getVisitById(id: number) {
+  const res = await axiosInstance.get(`/visits/${id}`)
+
+  return res.data
+}
+
 export async function createVisit(data: AddVisitFormData) {
   const res = await axiosInstance.post(`/visits`, data)
 
