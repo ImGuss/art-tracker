9d86d6b2a936a35e43bb3c9597347c1f72e8129a
Gustavo Gutierrez <imguss@gmail.com>
2026-05-07 23:58:27 -0400

    added visit detail interface

--- client/src/types/visit.ts
+++ client/src/types/visit.ts
@@ -1,3 +1,5 @@
+import type { Artwork } from './artwork'
+
 export interface Visit {
   id: number;
   user_id: number;
@@ -10,8 +12,12 @@ export interface VisitArtwork {
   artwork_id: number;
 }
 
+export interface VisitDetail extends Visit {
+  artworks: Artwork[];
+}
+
 export interface AddVisitFormData {
   user_id: number;
   museum_id: number;
-  visit_date: number;
+  visit_date: string;
 }
