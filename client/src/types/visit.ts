271e3d73364b6519dbeed6105d0b16c98c1b8f02
Gustavo Gutierrez <imguss@gmail.com>
2026-05-09 16:56:10 -0400

    changed museum name and visit date to string

--- client/src/types/visit.ts
+++ client/src/types/visit.ts
@@ -4,7 +4,8 @@ export interface Visit {
   id: number;
   user_id: number;
   museum_id: number;
-  visit_date: Date;
+  museum_name: string;
+  visit_date: string;
 }
 
 export interface VisitArtwork {
