4fd9f9ea2a43dd62c423960872c70a2df1016904
Gustavo Gutierrez <imguss@gmail.com>
2026-05-11 23:22:36 -0400

    added gallery card for visits and collections

--- client/src/components/GalleryCard.tsx
+++ client/src/components/GalleryCard.tsx
@@ -5,26 +5,49 @@ import type { Visit } from '../types/visit'
 import './GalleryCard.css'
 
 interface GalleryCardProps {
-  visit: Visit
+  visit: Visit;
+  // collection: Collection | null;
 }
 
 const GalleryCard = ({ visit }: GalleryCardProps) => {
+  const thumbnails = visit.artwork_thumbnails.slice(0, 4)
 
-  const renderArtworks = visit.artwork_thumbnails.map(artwork => {
+  const renderArtworks = thumbnails.map(artwork => {
     return (
-      <div key={artwork.id} className="mosaic-image">
-        <img 
-          src={artwork.image_url ?? undefined}
-          alt={artwork.title}
-        />
-      </div>
+      <img
+        key={artwork.id}
+        className="mosaic-image"
+        src={artwork.image_url ?? undefined}
+        alt={artwork.title}
+      />
     )
   })
 
   return (
-    <article className="card">
-      <div className="gallery-artwork-mosaic">
+    <article className="card gallery-card">
+      <div className={`gallery-card-mosaic count-${renderArtworks.length}`}>
+        {
+          renderArtworks.length > 0 ?
+          renderArtworks :
+          <p className="no-artwork-thumbnails">No artworks saved</p>
+        }
+      </div>
 
+      <h2>{visit.museum_name}</h2>
+
+      <div className="date-link">
+        <span>
+          {
+            new Date(visit.visit_date).toLocaleDateString('en-US', {
+              month: 'short',
+              day: 'numeric',
+              year: 'numeric'
+            })
+          }
+        </span>
+        <Link className="gold-link" to={`/visits/${visit.id}`}>
+          {visit.artwork_thumbnails.length} artworks
+        </Link>
       </div>
     </article>
   )
