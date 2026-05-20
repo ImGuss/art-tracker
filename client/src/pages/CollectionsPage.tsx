e48a8fd81011ac224ba1e79d5c3339877ade82b7
Gustavo Gutierrez <imguss@gmail.com>
2026-05-20 00:00:01 -0700

    started on collections logic

--- client/src/pages/CollectionsPage.tsx
+++ client/src/pages/CollectionsPage.tsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from 'react'
+
+import type { Collection } from '../types/collection'
+
+import { getUserCollections } from '../api/collectionApi'
+
 const CollectionsPage = () => {
+  const [collections, setCollections] = useState<Collection | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const [isLoading, setIsloading] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsloading(true)
+
+        const res = await getUserCollections()
+
+        setCollections(res)
+      } catch (err) {
+        setError('Error finding collections')
+      } finally {
+        setIsloading(false)
+      }
+    })()
+  }, [])
+
   return (
     <div>Collections Page</div>
   )
