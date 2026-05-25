3f2f2e896a7067d40060a1b3a5a057a6635daadb
Gustavo Gutierrez <imguss@gmail.com>
2026-05-25 15:36:19 -0700

    added get collection, add artwork, remove artwork, and started create collection

--- client/src/api/collectionApi.ts
+++ client/src/api/collectionApi.ts
@@ -5,3 +5,27 @@ export async function getUserCollections() {
 
   return res.data
 }
+
+export async function getCollectionById(id: number) {
+  const res = await axiosInstance.get(`/collections/${id}`)
+
+  return res.data
+}
+
+export async function createCollection() {
+  const res = await axiosInstance.post(`/collections`)
+
+  return res.data
+}
+
+export async function addArtworkToCollection(collectionId: number, artworkId: number) {
+  const res = await axiosInstance.post(`/collections/${collectionId}/artworks/${artworkId}`)
+
+  return res.data
+}
+
+export async function removeArtworkFromCollection(collectionId: number, artworkId: number) {
+  const res = await axiosInstance.delete(`/collections/${collectionId}/artworks/${artworkId}`)
+
+  return res.data
+}
