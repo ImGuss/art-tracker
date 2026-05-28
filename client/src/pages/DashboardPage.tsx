f9f97d037c18bb284352d60a0da46a6cd83cf37a
Gustavo Gutierrez <imguss@gmail.com>
2026-05-27 21:39:58 -0700

    started on dashboard logic

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 import { useAuth } from '../hooks/useAuth'
 
-const DashboardPage = () => {
+import { getUserVisits } from '../api/visitApi'
+import { getUserCollections } from '../api/collectionApi'
 
-  const user = useAuth().user
+import type { Visit } from '../types/visit'
+import type { Collection } from '../types/collection'
+
+const DashboardPage = () => {
+  const { user } = useAuth()
+
+  const [visits, setVisits] = useState<Visit[]>([])
+  const [collections, setCollections] = useState<Collection[]>([])
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const [visits, collections] = await Promise.all([getUserVisits(), getUserCollections()])
+  
+      })()
+    } catch (err) {
+      setError('Failed to get user data') 
+    }
+  }, [])
 
   if (!user) {
+    // will work on logic for logged out user after logged in user logic
     return(
       <div>
         Dashboard Page
@@ -13,6 +35,8 @@ const DashboardPage = () => {
       </div>
     )
   }
+
+
 
   return (
     <div>{user.username}'s Dashboard</div>
--- todo.md
+++ todo.md
@@ -1,5 +1,9 @@
-collection details
+dashboard page
 
-forms
-  add visit
-  add collection
+
+down the road
+
+- allow adding to collection from wherever an artwork card is shown
+- add links to the artwork detail tag pills that gets you to other similarly tagged artworks
+- search function on artist, artwork, and museum pages
+
