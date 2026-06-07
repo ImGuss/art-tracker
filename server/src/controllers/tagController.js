fcf31643a67e77b613ba4f12fc65f537bbef7251
Gustavo Gutierrez <imguss@gmail.com>
2026-06-07 12:53:02 -0700

    added pagination to artworks by tag

--- server/src/controllers/tagController.js
+++ server/src/controllers/tagController.js
@@ -18,12 +18,14 @@ export async function getTag(req, res, next) {
   try {
     
     const id = Number(req.params.id)
+    const limit = parseInt(req.query.limit, 10) || 20
+    const offset = parseInt(req.query.offset, 10) || 0
 
     if (isNaN(id)) {
       return next(new AppError('Invalid tag id', 400))
     }
 
-    const tag = await getTagById(id)
+    const tag = await getTagById(id, limit, offset)
 
     if (!tag) {
       return next(new AppError('Tag not found', 404))
--- server/src/models/tagModel.js
+++ server/src/models/tagModel.js
@@ -11,26 +11,38 @@ export async function getAllTags() {
   return res.rows
 }
 
-export async function getTagById(id) {
+export async function getTagById(id, limit, offset) {
   const res = await pool.query(
     `
+      WITH paginated_artworks AS (
+        SELECT
+          awt.tag_id,
+          aw.id AS artwork_id,
+          aw.title,
+          aw.image_url,
+          a.name AS artist_name
+        FROM artwork_tags awt
+        JOIN artworks aw ON awt.artwork_id = aw.id
+        LEFT JOIN artists a ON aw.artist_id = a.id
+        WHERE awt.tag_id = $1
+        ORDER BY aw.id DESC
+        LIMIT $2 OFFSET $3
+      )
       SELECT
         t.id,
         t.name AS tag_name,
         COALESCE(JSON_AGG(
           json_build_object(
-            'artwork_id', aw.id,
-            'title', aw.title,
-            'image_url', aw.image_url,
-            'artist_name', a.name
-        )) FILTER (WHERE aw.id IS NOT NULL), '[]') AS artworks
+            'artwork_id', pa.artwork_id,
+            'title', pa.title,
+            'image_url', pa.image_url,
+            'artist_name', pa.artist_name
+        )) FILTER (WHERE pa.artwork_id IS NOT NULL), '[]') AS artworks
       FROM tags t
-      LEFT JOIN artwork_tags awt ON awt.tag_id = t.id
-      LEFT JOIN artworks aw ON awt.artwork_id = aw.id
-      LEFT JOIN artists a ON aw.artist_id = a.id
+      LEFT JOIN paginated_artworks pa ON pa.tag_id = t.id
       WHERE t.id = $1
       GROUP BY t.id, t.name
-    `, [id]
+    `, [id, limit, offset]
   )
 
   return res.rows[0] || null
